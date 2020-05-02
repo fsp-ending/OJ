44981fc91b9625341e56cfb6f3d8a42e9895f851
fsp-ending <fsp-ending@1875346431@qq.com>
2020-05-02 21:21:27 +0800

    443 OJ

---
+++ 443.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: 443.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年05月02日 星期六 20时15分30秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+#define max_n 10000
+int arr[max_n + 5] = { 0 };
+
+void change_flag(int a, int b) {
+    for (int i = a; i <= b; i++) {
+        arr[i] = 1;
+    }
+    return ;
+}
+
+int cnt_num(int l) {
+    int cnt = 0;
+    for (int i = 0; i <= l; i++) {
+        !arr[i] && (cnt += 1);
+    }
+    return cnt;
+}
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        change_flag(a, b);
+    }
+    cout << cnt_num(m) << endl;
+
+
+    return 0;
+}
