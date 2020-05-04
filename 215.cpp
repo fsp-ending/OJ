f514d757e435d1a68dd81332045678bd6b015c1d
fsp-ending <1875346431@qq.com>
2020-05-04 13:13:52 +0800

    215 OJ

---
+++ 215.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 215.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年05月02日 星期六 12时07分42秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main() {
+    int n, num[4] = { 0 };
+    char  
+
+    return 0;
+}
