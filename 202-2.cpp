cf9f83554e758f71ed52f7c364709a69eee4fc04
fsp-ending <1875346431@qq.com>
2020-05-04 13:31:54 +0800

    202-2 OJ

---
+++ 202-2.cpp
@@ -0,0 +1,43 @@
+/*************************************************************************
+	> File Name: 202-2.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年05月04日 星期一 13时31分26秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+#define swap(a, b) { \
+    __typeof(a) __temp = a;\
+    a = b; b = __temp;\
+}
+
+int main() {
+    int n, num[1005] = { 0 };
+    int num2[1005] = { 0 };
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> num[i];
+        num2[i] = i;
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            if (num[j] > num[j + 1]) {
+                swap(num[j], num[j + 1]);
+                swap(num2[j], num2[j + 1]);
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        if (i != 1) {
+            cout << " ";
+        }
+        cout << num2[i];
+    }
+    cout << endl;
+    
+
+    return 0;
+}
+
