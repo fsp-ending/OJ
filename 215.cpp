7c0f336f45f6d3ca6e24ba9d13c8593be456da52
fsp-ending <1875346431@qq.com>
2020-05-04 14:21:54 +0800

    215 OJ

--- 215.cpp
+++ 215.cpp
@@ -6,12 +6,35 @@
  ************************************************************************/
 
 #include<iostream>
+#include <algorithm>
 #include<cstdio>
 using namespace std;
 
-int main() {
-    int n, num[4] = { 0 };
-    char  
+struct student {
+    int sum;
+    string name;
+};
 
-    return 0;
+bool cmp (student a, student b) {
+    return a.sum > b.sum;
+}
+
+int main() {
+
+    student stu[50];
+    int n;
+    cin >> n;
+    for (int i =0; i < n; i++) {
+        cin >> stu[i].name;
+        stu[i].sum = 0;
+        for (int j = 0; j < 4; j++) {
+            int temp;
+            cin >> temp;
+            stu[i].sum += temp;
+        }
+    }
+    sort(stu, stu + n, cmp);
+    for (int i = 0; i < 3; i++) {
+        cout << stu[i].name << endl;
+    }
 }
